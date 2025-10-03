ef0324af0cac0ec10c7126590608d8e8bd838883
Abdullah Sher Muhammad <147991640+AbdullahSherMuhammad@users.noreply.github.com>
2025-10-03 18:03:28 +0500

    button contact updated

--- src/app/page.js
+++ src/app/page.js
@@ -620,8 +620,8 @@ export default function Home() {
             that move your business forward.
           </p>
 
-          <div className="mt-8 relative z-10">
-            <Link href="/contact" className="inline-flex w-[215px] h-[50px] bg-[#65C8E3] text-white text-[20px] font-semibold rounded-full hover:bg-[#65C8E3] transition-colors duration-200 items-center justify-center"
+          <div className="mt-8 relative z-10 inline-flex w-[215px] h-[50px] bg-[#65C8E3] text-white text-[20px] font-semibold rounded-full hover:bg-[#65C8E3] transition-colors duration-200 items-center justify-center">
+            <Link href="/contact" className=""
               style={{ fontFamily: "Poppins, sans-serif" }}
             >
               Get In Touch
